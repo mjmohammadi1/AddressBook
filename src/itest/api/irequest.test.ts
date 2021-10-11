535e8d5bff094f80852afab92af980ebc30d2b44
Jafar <mjmohammadi1@gmail.com>
2021-10-11 16:23:28 -0400

    refactor: clean up tests

--- src/itest/api/irequest.test.ts
+++ src/itest/api/irequest.test.ts
@@ -1,16 +1,15 @@
 import { HTTP_METHODS, HTTP_STATUS_CODES } from '../../types/api';
 import request from '../../api/request';
+import { USER_QUERIES } from '../../utils/queries';
 
 describe('request test suite', () => {
+  const pageNumber = 1;
   test('successful api call returns 5  results', async () => {
     const {
       status,
       data: { results },
-    } = await request(HTTP_METHODS.GET, '?results=5');
+    } = await request(HTTP_METHODS.GET, USER_QUERIES.ALL_USERS + pageNumber);
     expect(status).toBe(HTTP_STATUS_CODES.OK);
-    expect(results.length).toBe(5);
+    expect(results.length).toBe(10);
   });
-  //TODO get data for a specific user
-  //TODO unit test for the request module
-  //config for unit and integration tests
 });
